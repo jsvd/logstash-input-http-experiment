282528f7e78aa7621d863b40b17cce4e316a5cc4
Joao Duarte <jsvduarte@gmail.com>
2020-01-28 10:54:50 +0000

    doc fix

--- ci/docker-setup.sh
+++ ci/docker-setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This is intended to be run the plugin's root directory. `ci/unit/docker-test.sh`
+# This is intended to be run the plugin's root directory.
 # Ensure you have Docker installed locally and set the ELASTIC_STACK_VERSION environment variable.
 set -e
 
